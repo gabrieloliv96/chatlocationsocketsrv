1f0a15d62bd3fcebc1b6334c11cb5cc4c8e8bc64
Gabriel Campelo <gabrielolivcamp96@gmail.com>
2025-02-24 14:24:42 -0300

    Finalização

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const server = http.createServer();
 const io = socketIO(server);
 
 const clients = [];
-const locations = [];
+let locations = [];
+const visibles = [];
 
 io.on("connection", (socket) => {
   clients.push(socket);
@@ -14,8 +15,21 @@ io.on("connection", (socket) => {
   socket.on("sendLocation", (data) => {
       console.log('Localização recebida');
       // console.log(data);
-      locations.push({socketId: socket.id, name: data['name'] ,coordx: data['coordx'], coordy: data['coordy']});
+      const hasLoc = locations.find(location => location.socketId == socket.id);
+      if(hasLoc !== undefined){
+        locations = locations.map(item => {
+          if (item.socketId === socket.id) {
+            // Se o socketId for igual, atualize as coordenadas
+            item.coordx = data['coordx'];
+            item.coordy = data['coordy'];
+          }
+          return item; // Retorna o item (modificado ou não)
+        });
+      } else {
+        locations.push({socketId: socket.id, name: data['name'] ,coordx: data['coordx'], coordy: data['coordy']});
+      }
       // Vendo se vai conectar com alguem
+      console.log(locations);
 
       console.log('Enviando a localização para proximos');
       const otherClient = findClient(socket);
@@ -27,11 +41,42 @@ io.on("connection", (socket) => {
         otherClient.forEach(client => {
           receiveClientLocation = locations.find(senderLocation => senderLocation.socketId == client.id);
           if(receiveClientLocation !== undefined) {
-              if(calcularDistancia(sendClientLocation.coordx, sendClientLocation.coordy, receiveClientLocation.coordx, receiveClientLocation.coordy)){
-                console.log('Client proximo encontrado.');
-                client.emit("new-user", `${data['name']}`);
-                sendClient.emit("new-user",`${receiveClientLocation.name}`);
+            if(calcularDistancia(sendClientLocation.coordx, sendClientLocation.coordy, receiveClientLocation.coordx, receiveClientLocation.coordy)){
+              console.log('Client proximo encontrado.');
+              // Verifica se a chave já existe no objeto 'visibles'
+              if(visibles[receiveClientLocation.name] !== undefined){
+                // Se existir, adiciona o nome na lista 'closer'
+                visibles[receiveClientLocation.name]['closer'].push(data['name']);
+              } else {
+                // Se não existir, cria uma nova entrada no objeto
+                visibles[receiveClientLocation.name] = {name: receiveClientLocation.name, closer: [data['name']]};
+              }
+              if(visibles[data['name']] !== undefined){
+                visibles[data['name']]['closer'].push(receiveClientLocation.name);
+              } else {
+                visibles[data['name']] = {name: data['name'], closer: [receiveClientLocation.name]};
+              }
+              console.log(visibles);
+
+              client.emit("new-user", `${data['name']}`);
+              sendClient.emit("new-user",`${receiveClientLocation.name}`);
+            } else {
+              if (visibles[receiveClientLocation.name] !== undefined) {
+                const index = visibles[receiveClientLocation.name]['closer'].indexOf(data['name']);
+                if (index !== -1) {
+                  visibles[receiveClientLocation.name]['closer'].splice(index, 1); // Remove o nome
+                }
               } 
+              
+              // Remover de 'closer' na entrada de 'data['name']'
+              if (visibles[data['name']] !== undefined) {
+                const index = visibles[data['name']]['closer'].indexOf(receiveClientLocation.name);
+                if (index !== -1) {
+                  visibles[data['name']]['closer'].splice(index, 1); // Remove o nome
+                }
+              }
+            }
+            console.log(visibles);
           }
         });
       } else {
@@ -46,12 +91,12 @@ io.on("connection", (socket) => {
     const sendClientLocation = locations.find(senderLocation => senderLocation.socketId == socket.id);
     otherClient.forEach(client => {
       const receiveClientLocation = locations.find(senderLocation => senderLocation.socketId == client.id);
-
+      if(receiveClientLocation.name )
       if(calcularDistancia(sendClientLocation.coordx, sendClientLocation.coordy, receiveClientLocation.coordx, receiveClientLocation.coordy)){
         client.emit("message", `${sendClientLocation.name}: ${data}`);
       } else {
         // Guarda a mensagem
-
+        
       }
 
     });
